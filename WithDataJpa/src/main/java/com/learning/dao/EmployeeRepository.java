58538e3c8e320a5025f6607a371585d85e8ade88
Santosh Kumar Kar <skkar.2k2@gmail.com>
2019-09-29 18:02:38 +0530

    Added few extra methods

--- WithDataJpa/src/main/java/com/learning/dao/EmployeeRepository.java
+++ WithDataJpa/src/main/java/com/learning/dao/EmployeeRepository.java
@@ -4,12 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learning.model.Employee;
 
+import java.util.List;
+
 /**
  * 
  * @author santoshkumarkar
  *
  */
+//Not a class
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-	
+    Employee findByEmail(String email);
+    List<Employee> findByEmailIn(List<String> emails);
+    List<Employee> findByNameAndAndDeptId(String name, Long deptId);
+    List<Employee> findByDeptIdNotIn(List<Long> ids);
 }
+
+
--- WithDataJpa/src/main/java/com/learning/service/EmployeeServiceImpl.java
+++ WithDataJpa/src/main/java/com/learning/service/EmployeeServiceImpl.java
@@ -57,4 +57,6 @@ public class EmployeeServiceImpl implements EmployeeService  {
 		empRepository.delete(dbEmp);
 		
 	}
+
+
 }
